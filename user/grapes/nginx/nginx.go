976b682f5b94848841aa89c51b67c15832489bd2
Stanislav Seletskiy <s.seletskiy@office.ngs.ru>
2014-03-02 17:28:04 +0700

    nginx grape

--- user/grapes/nginx/nginx.go
+++ user/grapes/nginx/nginx.go
@@ -6,11 +6,13 @@ import (
 
 func (p *Config) Ensure(cfg map[string]string) error {
 	p := new(api.Package)
-	//c := new(api.Config)
+	c := new(api.Config)
 	
 	if err := p.Ensure(map[string]string{"package": "nginx"}); err != nil {
-		log.Println("error: " + err.Error())
+		log.Println("nginx package error: " + err.Error())
 	}
 
-	//if err := c.Ensure(map[string]string{""})
+	if err := c.Ensure(cfg); err != nil {
+		log.Println("nginx config error: " + err.Error())
+	}
 }
