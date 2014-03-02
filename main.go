fd5bdea96ac7b1f69730c6f3009d5bb72f253048
Stanislav Seletskiy <s.seletskiy@office.ngs.ru>
2014-03-02 10:08:48 +0700

    gossip broadcasting with binary hotswapping

---
+++ main.go
@@ -0,0 +1,124 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+	"os"
+	"log"
+	"time"
+	"syscall"
+	"io"
+	"io/ioutil"
+	//"bufio"
+	//"strings"
+
+	"gossip"
+	"httpapi"
+)
+
+type ImmediateExecutor struct{}
+
+func (ie *ImmediateExecutor) Run(binStream io.Reader, net *gossip.Network) {
+	// @FIXME
+	binPath := os.Args[0]
+	log.Printf("replacing binary %s", binPath)
+	oldInfo, err := os.Stat(binPath)
+	if err != nil {
+		panic("error retrieving binary mode: " + err.Error())
+	}
+	tmpBinPath := binPath + ".new"
+	binFile, err := os.Create(tmpBinPath)
+	if err != nil {
+		panic("error creating new binary: " + err.Error())
+	}
+	for {
+		buf := make([]byte, 255)
+		n, _ := binStream.Read(buf)
+		if n == 0 {
+			break
+		}
+		binFile.Write(buf[:n])
+	}
+
+	binFile.Close()
+
+	os.Chmod(tmpBinPath, oldInfo.Mode())
+
+	stateFile, err := ioutil.TempFile(os.TempDir(), "grape-")
+	if err != nil {
+		panic("can't create state transient file")
+	}
+
+	for _, m := range net.GetMembers() {
+		fmt.Fprintf(stateFile, "%s:%d\n", m.Addr, m.Port)
+	}
+
+	stateFile.Close()
+
+	err = os.Rename(tmpBinPath, binPath)
+	if err != nil {
+		panic("error replacing old binary: " + err.Error())
+	}
+
+	panic(syscall.Exec(binPath,
+		[]string{
+			os.Args[0], os.Args[1], os.Args[2],
+			fmt.Sprintf("%d", net.GetVersion()),
+			stateFile.Name(),
+		},
+		os.Environ()))
+}
+
+func main() {
+	gossipPort, _ := strconv.Atoi(os.Args[1])
+	webPort, _ := strconv.Atoi(os.Args[2])
+	ver, _ := strconv.Atoi(os.Args[3])
+	log.Println("running at v.", ver)
+
+	conf := gossip.Config{
+		RootNodes: []string{
+			"127.1:2001",
+		},
+		LocalPort: gossipPort,
+		LocalVersion: int64(ver),
+		Name: fmt.Sprintf("%d", gossipPort),
+	}
+
+	net := gossip.NewGossipNetwork(conf, &ImmediateExecutor{})
+	
+	if len(os.Args) > 4 {
+		fmt.Println(os.Args)
+		stateFile, err := os.Open(os.Args[4])
+		if err != nil {
+			panic("error opening state file for reading: " + err.Error())
+		}
+		//reader := bufio.NewReader(stateFile)
+		for {
+			var line string
+			_, err := fmt.Fscanf(stateFile, "%s\n", &line)
+			if err != nil {
+				break
+			}
+
+			log.Printf("state: %s", line)
+
+			n, _ := net.Join(line)
+			if n > 0 {
+				log.Println("member joined again: ", line)
+			}
+		}
+	}
+
+	api := httpapi.Start(webPort)
+
+	api.UploadImage(ver, os.Args[0])
+	net.SendUpdateMsg(int64(ver), api.GetImageURI())
+
+	for {
+		for _, m := range net.GetMembers() {
+			fmt.Printf("[%s] %s:%d\n", m.Name, m.Addr, m.Port)
+		}
+
+		time.Sleep(5 * time.Second)
+	}
+}
---
+++ src/gossip/gossip.go
@@ -0,0 +1,201 @@
+package gossip
+
+import (
+	"encoding/json"
+	//"time"
+	"io"
+	"net/http"
+	//"os/exec"
+	//"syscall"
+	"bufio"
+	"log"
+	"fmt"
+	"github.com/hashicorp/memberlist"
+)
+
+var _ = fmt.Println
+
+type UpdateExecutor interface {
+	Run(binStream io.Reader, net *Network)
+}
+
+type Config struct {
+	RootNodes []string
+	LocalPort int
+	LocalVersion int64
+	Name string
+}
+
+type Broadcast struct {
+	rate int
+	msg []byte
+}
+
+type Network struct {
+	members *memberlist.Memberlist
+	broadcasts []*Broadcast
+	executor UpdateExecutor
+	version int64
+}
+
+func (d *Network) GetVersion() int64 {
+	return d.version
+}
+
+func (d *Network) NodeMeta(limit int) []byte {
+	return nil
+}
+
+func (d *Network) NotifyMsg(msg []byte) {
+	log.Printf("got broadcast: %s", msg)
+	var possibleUpdate UpdateMsg
+	if err := json.Unmarshal(msg, &possibleUpdate); err == nil {
+		if possibleUpdate.Version > d.version {
+			//log.Println("replicate broadcast due new version")
+			d.AddBroadcast(msg)
+			//d.broadcasts = append(d.broadcasts, msg)
+			d.version = possibleUpdate.Version
+			//d.updating = true
+
+			resp, err := http.Get(possibleUpdate.URI)
+			if err != nil {
+				log.Fatal(err)
+				return
+			}
+
+			d.executor.Run(bufio.NewReader(resp.Body), d)
+		}
+	}
+}
+
+func (d *Network) GetBroadcasts(overhead, limit int) [][]byte {
+	toSend := make([][]byte, 0)
+	keep := make([]*Broadcast, 0)
+	for _, b := range d.broadcasts {
+		if b.rate <= 0 {
+			continue
+		}
+
+		limit -= len(b.msg)
+		if limit < 0 {
+			break
+		}
+
+		b.rate -= 1
+
+		toSend = append(toSend, b.msg)
+		keep = append(keep, b)
+	}
+
+	if len(toSend) == 0 {
+		return nil
+	}
+
+	d.broadcasts = keep
+
+	log.Printf("sending broadcasts: %s", toSend)
+
+	return toSend
+}
+
+func (d *Network) LocalState(join bool) []byte {
+	return nil
+}
+
+func (d *Network) MergeRemoteState(buf []byte, join bool) {
+
+}
+
+func (d *Network) AddBroadcast(buf []byte) {
+	// @TODO Hardcoded retransmit rate!
+	d.broadcasts = append(d.broadcasts, &Broadcast{
+		rate: 3,
+		msg: buf,
+	})
+}
+
+//type EventNetwork struct {
+	
+//}
+
+//func (e *EventNetwork) NotifyJoin(*memberlist.Node)() {
+
+//}
+
+//func (e *EventNetwork) NotifyJoin(*memberlist.Node)() {
+
+//}
+
+//func (e *EventNetwork) NotifyJoin(*memberlist.Node)() {
+
+//}
+
+//type Msg interface {
+//    GetVersion() int64
+//}
+
+//type VersionMsg struct {
+//    gen int64
+//}
+
+//func (i VersionMsg) GetVersion() int64 {
+//    return i.gen
+//}
+
+//func NewVersionMsg() *VersionMsg {
+//    return &VersionMsg{
+//        time.Now().UnixNano(),
+//    }
+//}
+
+type UpdateMsg struct {
+	Version int64 `json:"v"`
+	URI string `json:"uri"`
+}
+
+func NewGossipNetwork(netConf Config, executor UpdateExecutor) *Network {
+	conf := memberlist.DefaultLocalConfig()
+	network := &Network{
+		executor: executor,
+		version: netConf.LocalVersion,
+	}
+
+	conf.BindPort = netConf.LocalPort
+	conf.Name = netConf.Name
+	conf.Delegate = network
+	
+	list, err := memberlist.Create(conf)
+	if err != nil {
+		panic("Failed to create memberlist: " + err.Error())
+	}
+
+	n := 0
+	for i := 0; i < 3; i++ {
+		n, err = list.Join(netConf.RootNodes)
+		if n > 0 {
+			break
+		}
+	}
+
+	if n == 0 {
+		panic("Can't connect to any of the root nodes: " + err.Error())
+	}
+
+	network.members = list
+
+	return network
+}
+
+func (net *Network) SendUpdateMsg(version int64, uri string) {
+	net.version = version
+	bin, _ := json.Marshal(UpdateMsg{version, uri})
+	net.AddBroadcast(bin)
+}
+
+func (net *Network) GetMembers() []*memberlist.Node {
+	return net.members.Members()
+}
+
+func (net *Network) Join(host string) (int, error) {
+	return net.members.Join([]string{host})
+}
